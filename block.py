5a9a8263c15b03b5bc319511e2e288542a56c25c
oversizehrt <78272265+oversizehrt@users.noreply.github.com>
2021-01-30 22:17:41 +0300

    Update block.py

--- block.py
+++ block.py
@@ -9,6 +9,11 @@ class Block:
         self.data = data
         self.previous_hash = previous_hash
         self.hash = self.hash_block()
+        
+        # I used .encode('utf-8') because Python 3 uses Unicode
+        # but .sha256() waits for UTF-8 code
+        # I did had this error:
+        # TypeError: Unicode-objects must be encoded before hashing
 
     def hash_block(self):
         hash = hashlib.sha256()
